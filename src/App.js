3c5923547be2ee5637d41bdbe978da1803488cdc
DeniseFafette <denisefafette@gmail.com>
2019-12-12 14:20:01 -0800

    added styles.css and updated App.js to view EarthQuake component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
+// remove boilerplate imports and jsx
+// import components and styles
+
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import EarthQuake from './components/EarthQuake';
+import './styles.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <EarthQuake />
     </div>
   );
 }
